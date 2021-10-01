8434e0ddfc87d5d39642ed66d444e2c45690e96d
alanr8742 <91750486+alanr8742@users.noreply.github.com>
2021-10-01 12:53:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Alan Rivera"; // update the part BETWEEN the "quotes"
+let userName = "alanr8742"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
